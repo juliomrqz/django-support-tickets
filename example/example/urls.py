82ccdb6934c5c41f5dd42a8b11a50e743f90b496
Julio Marquez <juliocmrqzm@gmail.com>
2016-07-18 10:06:21 -0400

    Fix media url in demo

--- example/example/urls.py
+++ example/example/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 import debug_toolbar
@@ -32,3 +34,8 @@ urlpatterns = [
     # Debug Toolbar
     url(r'^__debug__/', include(debug_toolbar.urls)),
 ]
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
