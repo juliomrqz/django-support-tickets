0864e944933183b9e132470eed0f2c727cbab728
Julio Marquez <juliocmrqzm@gmail.com>
2016-09-09 10:11:40 -0400

    Reset migrations

--- support_tickets/migrations/0001_initial.py
+++ support_tickets/migrations/0001_initial.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import migrations, models
-import support_tickets.base.fields
-import model_utils.fields
-import django_markup.fields
-import django.utils.timezone
 from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+import django.utils.timezone
+import django_markup.fields
+import model_utils.fields
+import support_tickets.base.fields
 
 
 class Migration(migrations.Migration):
+
+    initial = True
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
@@ -17,14 +20,33 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Attachment',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, editable=False, verbose_name='created')),
+                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, editable=False, verbose_name='modified')),
+                ('name', models.CharField(blank=True, max_length=80, verbose_name='File name')),
+                ('file', models.FileField(upload_to='support/tickets/attachments', verbose_name='Attachment')),
+                ('size', models.BigIntegerField(blank=True, help_text='File size in bytes', null=True, verbose_name='File Size')),
+            ],
+            options={
+                'ordering': ['-created'],
+                'verbose_name': 'Attachment',
+                'verbose_name_plural': 'Attachments',
+            },
+        ),
+        migrations.CreateModel(
             name='Category',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, verbose_name='created', editable=False)),
-                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, editable=False, verbose_name='created')),
+                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, editable=False, verbose_name='modified')),
                 ('title', models.CharField(max_length=100)),
-                ('slug', support_tickets.base.fields.CustomAutoSlugField(editable=False, populate_from=b'title', unique=True, slugify=support_tickets.base.fields.custom_slugify)),
-                ('default_agent', models.ForeignKey(related_name='default_agent', verbose_name='Default agent', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+                ('slug', support_tickets.base.fields.CustomAutoSlugField(editable=False, populate_from=b'title', slugify=support_tickets.base.fields.custom_slugify, unique=True)),
+                ('order', models.IntegerField(default=0)),
+                ('active', models.BooleanField(default=True)),
+                ('default_agent', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='default_agent', to=settings.AUTH_USER_MODEL, verbose_name='Default agent')),
+                ('parent', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='children_set', to='support_tickets.Category')),
             ],
             options={
                 'ordering': ['title'],
@@ -33,24 +55,58 @@ class Migration(migrations.Migration):
             },
         ),
         migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, editable=False, verbose_name='created')),
+                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, editable=False, verbose_name='modified')),
+                ('description', models.TextField(help_text='Include as much detail as possible, including the steps to reproduce, error message(s), screen shots, URLs, date/time/duration, etc. This information will accelerate our ability to help you.', verbose_name='Description')),
+                ('markup', django_markup.fields.MarkupField(choices=[(b'none', b'None (no processing)'), (b'linebreaks', b'Linebreaks'), (b'markdown', b'Markdown'), (b'restructuredtext', 'reStructuredText')], default='none', max_length=255, verbose_name='markup')),
+            ],
+            options={
+                'ordering': ['-created'],
+                'verbose_name': 'Comment',
+                'verbose_name_plural': 'Comments',
+            },
+        ),
+        migrations.CreateModel(
             name='Ticket',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, verbose_name='created', editable=False)),
-                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, editable=False, verbose_name='created')),
+                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, editable=False, verbose_name='modified')),
                 ('subject', models.CharField(help_text='Type a brief summary of your question or issue.', max_length=100, verbose_name='Subject')),
-                ('status', models.IntegerField(default=0, verbose_name='Status', choices=[(0, 'Open'), (1, 'Reopened'), (2, 'Resolved'), (3, 'Closed'), (4, 'Duplicate')])),
-                ('priority', models.IntegerField(default=3, help_text='1 = Highest Priority, 5 = Low Priority', verbose_name='Priority', choices=[(1, '1. Critical'), (2, '2. High'), (3, '3. Normal'), (4, '4. Low'), (5, '5. Very Low')])),
-                ('description', models.TextField(help_text='Include as much detail as possible, including the steps to reproduce, error message(s), screen shots, URLs, date/time/duration, etc. This information will accelerate our ability to help you.', verbose_name='Description')),
-                ('markup', django_markup.fields.MarkupField(default='markdown', max_length=255, verbose_name='markup', choices=[(b'none', b'None (no processing)'), (b'linebreaks', b'Linebreaks'), (b'markdown', b'Markdown'), (b'restructuredtext', 'reStructuredText')])),
-                ('agent', models.ForeignKey(related_name='tickets', verbose_name='Agent', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
-                ('category', models.ForeignKey(verbose_name='Category', to='support_tickets.Category')),
-                ('submitter', models.ForeignKey(related_name='submitter', verbose_name='Submitter', to=settings.AUTH_USER_MODEL)),
+                ('status', models.IntegerField(choices=[(0, 'Open'), (1, 'Reopened'), (2, 'Resolved'), (3, 'Closed'), (4, 'Duplicate')], default=0, verbose_name='Status')),
+                ('priority', models.IntegerField(choices=[(1, 'Critical'), (2, 'High'), (3, 'Normal'), (4, 'Low'), (5, 'Very Low')], default=3, help_text='1 = Highest Priority, 5 = Lowest Priority', verbose_name='Priority')),
+                ('last_active', models.DateTimeField(default=django.utils.timezone.now, verbose_name='Last active')),
+                ('agent', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='tickets', to=settings.AUTH_USER_MODEL, verbose_name='Agent')),
+                ('category', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='support_tickets.Category', verbose_name='Category')),
+                ('submitter', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='submitter', to=settings.AUTH_USER_MODEL, verbose_name='Submitter')),
             ],
             options={
                 'ordering': ['created'],
                 'verbose_name': 'Ticket',
                 'verbose_name_plural': 'Tickets',
             },
+        ),
+        migrations.AddField(
+            model_name='comment',
+            name='ticket',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='comments', to='support_tickets.Ticket', verbose_name='Ticket'),
+        ),
+        migrations.AddField(
+            model_name='comment',
+            name='user',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='comments', to=settings.AUTH_USER_MODEL, verbose_name='User'),
+        ),
+        migrations.AddField(
+            model_name='attachment',
+            name='comment',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='attachments', to='support_tickets.Comment', verbose_name='Comment'),
+        ),
+        migrations.AddField(
+            model_name='attachment',
+            name='uploader',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='attachments', to=settings.AUTH_USER_MODEL, verbose_name='Uploader'),
         ),
     ]
